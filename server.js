568fb6d1e4467d6006f7695e84263fae8f7c257e
TilakKawa <kawatilak06@gmail.com>
2023-02-09 14:32:48 -0500

    updated server.js

--- server.js
+++ server.js
@@ -1,3 +1,14 @@
+/*********************************************************************************
+*  WEB322 – Assignment 02
+*  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.  No part *  of this assignment has been copied manually or electronically from any other source 
+*  (including 3rd party web sites) or distributed to other students.
+* 
+*  Name: Tilak Rajeshkumar Kawa_ Student ID: 164542219_ Date: 08th Feb 2023__
+*
+*  Online (Cyclic) Link: 
+*
+********************************************************************************/ 
+
 var express = require("express");
 var path = require("path");
 var app = express();
